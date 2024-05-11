feadbf2131807022f50634aede6cd9767a72c6a1
joycellu <jol072@ucsd.edu>
2024-05-10 17:08:38 -0700

    added hover over effect and basic tooltip

--- vanilla/script.js
+++ vanilla/script.js
@@ -15,6 +15,11 @@ let colorScale = d3.scaleThreshold()
   .domain([10, 20, 50, 100, 200, 500])
   .range(d3.schemeBlues[7]);
 
+let tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
+  .style("position", "absolute");
+
 d3.queue()
     .defer(d3.json, "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json")
     .defer(d3.csv, "https://raw.githubusercontent.com/fivethirtyeight/data/master/bad-drivers/bad-drivers.csv", function(d) {
@@ -22,20 +27,38 @@ d3.queue()
     })
     .await(ready);
   
-  function ready(error, topo) {
-    if (error) throw error;
-  
-    // Draw the map
-    svg.append("g")
-      .selectAll("path")
-      .data(topo.features)
-      .enter()
-      .append("path")
-      // draw each state
-      .attr("d", d3.geoPath().projection(projection))
-      // set the color of each state
-      .attr("fill", function(d) {
-        d.total = data.get(d.properties.name) || 0;
-        return colorScale(d.total);
-      });
+function ready(error, topo) {
+  if (error) throw error;
+
+  // Draw the map
+  svg.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .enter()
+    .append("path")
+    // draw each state
+    .attr("d", d3.geoPath().projection(projection))
+    // set the color of each state
+    .attr("fill", function(d) {
+      d.total = data.get(d.properties.name) || 0;
+      return colorScale(d.total);
+    })
+
+    .on("mouseover", function(d) {
+      d3.select(this).style("fill", "gray");
+      tooltip.transition()
+              .duration(200)
+              .style("opacity", .9);
+          tooltip.html("tooltip")  // change tooltip displayed info
+              .style("left", (d3.event.pageX + 10) + "px")
+              .style("top", (d3.event.pageY - 28) + "px");
+    })
+    
+
+    .on("mouseout", function(d) {
+      d3.select(this).style("fill", colorScale(d.total));
+      tooltip.transition()
+              .duration(200)
+              .style("opacity", 0);
+    });
   }
